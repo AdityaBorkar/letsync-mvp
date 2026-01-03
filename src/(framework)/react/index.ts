d3d39a040ece57e246ba8eb3274870d51e2fcc54
Aditya Borkar <aditya.borkar.programs@gmail.com>
2026-01-03 12:41:22 +0000

    wip

--- src/(framework)/react/index.ts
+++ src/(framework)/react/index.ts
@@ -1,10 +1,10 @@
 /** biome-ignore-all lint/performance/noBarrelFile: THIS IS A LIBRARY */
 
-export { REPL } from "./components/repl.tsx"
-export { Studio } from "./components/studio.tsx"
+export { REPL } from "./components/repl.js"
+export { Studio } from "./components/studio.js"
 // export { useDatabase } from "./hooks/useDatabase.js"
 // export { useLiveQuery } from "./hooks/useLiveQuery.js"
 // export { useQuery } from "./hooks/useQuery.js"
 // export { useSync } from "./hooks/useSync.js"
 // export { useSyncStatus } from "./hooks/useSyncStatus.js"
-export { LetsyncProvider } from "./providor.tsx"
+export { LetsyncProvider } from "./providor.js"
