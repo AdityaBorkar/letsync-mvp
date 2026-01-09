927a675241cb68cfc1327415dd5299a603557610
Aditya Borkar <aditya.borkar.programs@gmail.com>
2026-01-09 16:03:59 +0000

    wip: refactor

--- src/(adapters)/ws-server-patch/index.ts
+++ src/(adapters)/ws-server-patch/index.ts
@@ -1,3 +1,2 @@
 /** biome-ignore-all lint/performance/noBarrelFile: This is a LIBRARY */
-
-export { PubSubClient } from "./client/index.js"
+export { WsServer } from "../websockets/server/index.js"
--- src/core/server/api-handler.ts
+++ src/core/server/api-handler.ts
@@ -1,17 +1,11 @@
 import { API_ENDPOINTS } from "./api-endpoints.js"
 import { Context, type ContextType } from "./utils/context.js"
-import { logger } from "./utils/logger.js"
 
 export async function apiHandler<R extends Request>(
   request: R,
   ctx: ContextType
 ) {
   Context.enterWith(ctx)
-  const context = Context.getStore()
-  if (!context) {
-    logger.error("Context not found")
-    return Response.json({ message: "Internal Server Error" }, { status: 500 })
-  }
 
   const url = new URL(request.url)
   const path = url.pathname as keyof typeof API_ENDPOINTS
